7959f53c767b51f79296a25a141b5b81cbb6d4ca
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-10-07 23:33:15 +0300

    added step to buy the first grandma

--- bot/bot.js
+++ bot/bot.js
@@ -3,8 +3,9 @@ const cookiePage = require("../pages/cookie.page");
 describe("Cookie Clicker Bot", function () {
   before(function () {
     cookiePage.openUrl();
-  })
-  it("Start", function () {
-    cookiePage.clickCookie();
+  });
+  it("Buy first grandma", function () {
+    cookiePage.clickToReach(cookiePage.grandMaPrice);
+    cookiePage.buyGrandMa();
   });
 });
--- pages/cookie.page.js
+++ pages/cookie.page.js
@@ -4,9 +4,12 @@ class CookiesPage {
     this.clickTimeout = 30;
   }
 
-  get topMenu() {
-    return $("#topBar");
+  openUrl() {
+    browser.url(this.url);
+    this.cookie.waitForDisplayed();
   }
+
+  //page + cookie block
   get cookie() {
     return $("#bigCookie");
   }
@@ -14,6 +17,7 @@ class CookiesPage {
     return $('#cookies');
   }
   get cookiesAmount() {
+    this.cookiesAmountBlock.scrollIntoView();
     const amount = this.cookiesAmountBlock.getText().split(" ")[0];
     return +amount;
   }
@@ -23,16 +27,63 @@ class CookiesPage {
     return +amount;
   }
 
-  openUrl() {
-    browser.url(this.url);
-    this.cookie.waitForDisplayed();
-  }
-
   clickCookie(times, clickTimeout = this.clickTimeout) {
-    for (let i = 0; i <= times; i++) {
+    this.cookie.scrollIntoView();
+    for (let i = 0; i < times; i++) {
       this.cookie.click();
       browser.pause(clickTimeout);
     }
+  }
+  clickToReach(price) {
+    this.cookie.scrollIntoView();
+    let curr = this.cookiesAmount;
+    while(curr < price) {
+      this.clickCookie(1);
+      curr = this.cookiesAmount;
+    }
+  }
+
+  //products
+  get productsBlock() {
+    return $("#products");
+  }
+  get cursor() {
+    return this.product(0);
+  }
+  get cursorPrice() {
+    return this.productPrice(0);
+  }
+  get cursorAmount() {
+    return this.productOwnedAmount(0);
+  }
+  get grandMa() {
+    return this.product(1);
+  }
+  get grandMaPrice() {
+    return this.productPrice(1);
+  }
+  get grandMaAmount() {
+    return this.productOwnedAmount(1);
+  }
+
+  product(index) {
+    return this.productsBlock.$(`#product${index}`);
+  }
+  productPrice(index) {
+    const amount = this.productsBlock.$(`#productPrice${index}`).getText();
+    return +amount;
+  }
+  productOwnedAmount(index) {
+    const amount = this.productsBlock.$(`#productOwned${index}`).getText();
+    return +amount;
+  }
+  buyCursor() {
+    this.cursor.scrollIntoView();
+    this.cursor.click();
+  }
+  buyGrandMa() {
+    this.grandMa.scrollIntoView();
+    this.grandMa.click();
   }
 }
 
