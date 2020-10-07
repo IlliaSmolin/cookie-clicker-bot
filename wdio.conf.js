3a4b113532657f49ba437a516d7e64d98fd28deb
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-10-07 22:00:30 +0300

    configured wdio

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './bot/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -90,7 +90,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://orteil.dashnet.org/cookieclicker/',
+    baseUrl: 'https://orteil.dashnet.org/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
