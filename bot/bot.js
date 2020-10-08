979660441703c6eae6cf43a3020d9f8cd8d64644
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-10-08 19:34:02 +0300

    added step to disable short numbers option

--- bot/bot.js
+++ bot/bot.js
@@ -1,6 +1,7 @@
 const cookiePage = require("../pages/cookie.page");
 const cookie = require("../components/cookie.component");
 const products = require("../components/products.component");
+const options = require("../components/options.component");
 const cookiesBanner = require("../helpers/cookiesBanner.helper");
 const { assert } = require("chai");
 
@@ -15,6 +16,9 @@ describe("Cookie Clicker Bot", function () {
   it("Accept cookies policy", function () {
     cookiesBanner.acceptCookies();
     assert.strictEqual(cookiesBanner.acceptCookiesBtn.isDisplayed(), false);
+  });
+  it("Disable short numbers option", function () {
+    options.disableShortNumbers();
   });
   it("Buy first grandma", function () {
     cookie.clickToReach(products.grandMaPrice, timeouts.click);
