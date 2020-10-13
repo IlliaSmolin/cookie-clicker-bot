c6fbe8ab33e43015882bbb42ee71b047d27cda70
IlliaSmolin <55273908+IlliaSmolin@users.noreply.github.com>
2020-10-13 21:53:18 +0300

    refactored local functions to reduce code repeatability

--- bot/bot.js
+++ bot/bot.js
@@ -12,18 +12,26 @@ let amounts = {
 },
   mousePower = 1;
 
-function buyCursor() {
+function reachAndBuyCursor() {
+  cookie.clickToReachCursor(mousePower);
   products.buyCursor();
   amounts.cursor++;
 }
-function buyGrandMa() {
+function reachAndBuyGrandMa() {
+  cookie.clickToReachGrandMa(mousePower);
   products.buyGrandMa();
   amounts.grandMa++;
 }
-function buyUpgrade(cursor = false) {
+function reachAndBuyUpgrade(cursor = false) {
+  const initialUpgradesAmount = upgrades.upgradesAmount;
+  cookie.clickToReachUpgrade(mousePower);
   upgrades.buyUpgrade();
   if (cursor) {
     mousePower *= 2;
+  }
+  return {
+    initItems: initialUpgradesAmount,
+    newItems: upgrades.upgradesAmount,
   }
 }
 
@@ -39,26 +47,22 @@ describe("Cookie Clicker Bot", function () {
     options.setHighPerformance();
   });
   it("Buy first grandma", function () {
-    cookie.clickToReachGrandMa(mousePower);
-    buyGrandMa();
+    reachAndBuyGrandMa();
     assert.strictEqual(products.grandMaAmount, amounts.grandMa);
   });
   it("Buy second grandma", function () {
-    cookie.clickToReachGrandMa(mousePower);
-    buyGrandMa();
+    reachAndBuyGrandMa();
     assert.strictEqual(products.grandMaAmount, amounts.grandMa);
   });
   it("Buy 5 cursors", function () {
     for (let i = 0; i < 5; i++) {
-      cookie.clickToReachCursor(mousePower);
-      buyCursor();
+      reachAndBuyCursor();
     }
     assert.strictEqual(products.cursorAmount, amounts.cursor);
   });
   it("Buy cursor upgrade", function () {
-    const initialUpgradesAmount = upgrades.upgradesAmount;
-    cookie.clickToReachUpgrade(mousePower);
-    buyUpgrade(true);
-    assert.equal(upgrades.upgradesAmount, initialUpgradesAmount - 1);
+    const { initItems, newItems } = reachAndBuyUpgrade(true);
+    console.log({ initItems, newItems });
+    assert.equal(newItems, initItems - 1);
   });
 });
