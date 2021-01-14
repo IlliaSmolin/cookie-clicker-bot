861d81a090d0e3582f66439bb16254907956abc5
Illia <smolinilusha@gmail.com>
2021-01-14 22:27:59 +0200

    added typescript compilation on fly to the global before hook

--- wdio.conf.js
+++ wdio.conf.js
@@ -180,8 +180,17 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // before: function (capabilities, specs) {
-    // },
+    before: function () {
+      const width = browser.execute(() => {
+        return window.screen.availWidth;
+      });
+      const height = browser.execute(() => {
+        return window.screen.availHeight;
+      });
+      browser.setWindowSize(width, height);
+
+      require('ts-node').register({ files: true });
+    },
     /**
      * Runs before a WebdriverIO command gets executed.
      * @param {String} commandName hook command name
